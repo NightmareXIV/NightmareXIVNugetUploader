fb7a608b06f97e2bc2b746071f3091361dd6bef2
Limiana <5073202+Limiana@users.noreply.github.com>
2025-06-28 03:34:19 +0300

    Update code

--- NightmareXIVNugetUploader/Program.cs
+++ NightmareXIVNugetUploader/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, World!\n{string.Join(",", Directory.GetFiles("."))}");
+            Console.WriteLine($"Hello, World!\n{string.Join("\n", Directory.GetFiles("."))}");
         }
     }
 }
